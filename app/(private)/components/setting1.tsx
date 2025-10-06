b27fb6d7f942b604bda6acdd7e07c0ae936cc4b5
Kartik Mishra <kartik26nov2004@gmail.com>
2025-10-06 15:39:49 +0530

    Fix setting UI.

--- app/(private)/components/setting1.tsx
+++ app/(private)/components/setting1.tsx
@@ -11,19 +11,11 @@ import { useState } from 'react'
 import { useContext } from 'react'
 import { logoutapi, registerApi } from '@/app/Apis/publicapi'
 
-
- 
-
-
-
-  
 const Setting1 = () => {
 
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const router = useRouter()
-
-
 
   const { setOpenSetting2 } = useContext<any>(SettingContext)
   const { setOpenSetting1 } = useContext<any>(SettingContext)
@@ -41,15 +33,12 @@ const Setting1 = () => {
   })
 
   const handlelogout = async () => {
-
-
     try {
       await logoutapi({});
       localStorage.removeItem("token");
       setOpenSetting1(false);
       setSnackbarMessage('Logout successful');
       setOpenSnackbar(true);
-
 
       setTimeout(() => {
         router.push("/login");
@@ -58,127 +47,103 @@ const Setting1 = () => {
       setSnackbarMessage(err.message || "Logout failed");
       setOpenSnackbar(true);
     }
-
   };
-  
 
-  
-  
-   const [registerData, setUser] = useState<any>()
- 
- 
-   useEffect(() => {
-     const storedUser = localStorage.getItem('userdata')
-     if (storedUser) {
-       setUser(JSON.parse(storedUser))
+  const [registerData, setUser] = useState<any>()
 
-       console.log(storedUser)
-     }
-     
- 
- 
-   }, [])
-
-
- 
- 
+  useEffect(() => {
+    const storedUser = localStorage.getItem('userdata')
+    if (storedUser) {
+      setUser(JSON.parse(storedUser))
+      console.log(storedUser)
+    }
+  }, [])
 
   return (
     <div>
-      <div className="fixed inset-0 flex items-center justify-center bg-[#9999] p-4 sm:p-6 select-none ">
+      <div className="fixed inset-0 flex items-center justify-center bg-[#9999] p-4 sm:p-6 select-none">
 
-        <div className="relative z-10  flex justify-center items-center w-full">
-          <div className="w-full max-w-[717px] h-[650px] opacity-100 border-[#E2E4E84D] bg-[#ffffff] rounded-[16px] border border-solid overflow-y-auto ">
+        <div className="relative z-10 flex justify-center items-center w-full">
+          <div className="w-full max-w-[800px] h-auto opacity-100 border-[#E2E4E84D] bg-[#ffffff] rounded-[16px] border border-solid">
 
             <section className="w-full h-auto flex justify-between items-center border-b border-[#F6F6F6] rounded-t-[16px] px-4 py-3 sm:px-[20px] sm:py-[12px]">
               <span className="w-auto font-inter font-semibold text-[18px] sm:text-[20px] leading-[150%] tracking-[-0.04em] text-[#1D2939]">
                 Settings
               </span>
-              <span className="w-9 h-9 rounded-full border p-2 flex items-center justify-center border-[#F1F2F3] bg-[#F6F8FA]" onClick={() => setOpenSetting1(false)} >
+              <span className="w-9 h-9 rounded-full border p-2 flex items-center justify-center border-[#F1F2F3] bg-[#F6F8FA]" onClick={() => setOpenSetting1(false)}>
                 <Icon icon="iconamoon:close-bold" width="24" height="24" style={{ color: '#000' }} />
               </span>
             </section>
 
-            <section className="w-full h-auto px-4 py-6 sm:px-[64px] sm:py-[32px] flex flex-col">
-              <div className="w-full flex flex-col gap-6">
+            <section className="w-full h-auto px-4 py-4 sm:px-[64px] sm:py-[24px] flex flex-col">
+              <div className="w-full flex flex-col gap-4">
                 <div className="w-full flex flex-wrap items-center gap-2 sm:gap-5">
                   <div className="flex flex-wrap gap-2 w-full">
-                    <button className="rounded-sm text-sm px-3 py-2 bg-[#F9F5FF] hover:cursor-pointer ">
-                      <span className="font-semibold text-[14px]  text-[#685BC7]    ">Profile</span>
+                    <button className="rounded-sm text-sm px-3 py-2 bg-[#F9F5FF] hover:cursor-pointer">
+                      <span className="font-semibold text-[14px] text-[#685BC7]">Profile</span>
                     </button>
                     <button className="rounded-sm text-sm px-3 py-2" onClick={() => {
                       setOpenSetting2(true)
                       setOpenSetting1(false)
                       setOpenSetting3(false)
-                    }} >
-                      <span className="font-semibold text-[14px] text-[#667085]  hover:text-[#685BC7] bg-[#F9F5FF] rounded-sm text-sm px-3 py-2 hover:cursor-pointer ">Security</span>
+                    }}>
+                      <span className="font-semibold text-[14px] text-[#667085] hover:text-[#685BC7] bg-[#F9F5FF] rounded-sm text-sm px-3 py-2 hover:cursor-pointer">Security</span>
                     </button>
-                    <button className="rounded-sm text-sm " onClick={() => {
+                    <button className="rounded-sm text-sm" onClick={() => {
                       setOpenSetting3(true)
                       setOpenSetting1(false)
                       setOpenSetting2(false)
-                    }}  >
-                      <span className="font-semibold text-[14px] text-[#667085]  hover:text-[#685BC7] bg-[#F9F5FF] rounded-sm text-sm px-3 py-2 hover:cursor-pointer ">Billing</span>
+                    }}>
+                      <span className="font-semibold text-[14px] text-[#667085] hover:text-[#685BC7] bg-[#F9F5FF] rounded-sm text-sm px-3 py-2 hover:cursor-pointer">Billing</span>
                     </button>
-                    <button className="rounded-sm text-sm px-3 py-2" onClick={handlelogout} >
-                      <span className="font-semibold text-[14px] text-[#667085]  hover:text-[red] bg-[white] rounded-sm text-sm px-3 py-2 hover:cursor-pointer ">Log Out</span>
+                    <button className="rounded-sm text-sm px-3 py-2" onClick={handlelogout}>
+                      <span className="font-semibold text-[14px] text-[#667085] hover:text-[red] bg-[white] rounded-sm text-sm px-3 py-2 hover:cursor-pointer">Log Out</span>
                     </button>
                   </div>
                 </div>
                 <div className="text-[18px] font-semibold text-[#101828]">Account Profile</div>
               </div>
 
-              <div className="w-full flex flex-col gap-4">
-                <div className="flex flex-col sm:flex-row items-center gap-4">
-                  <img src="/userse.png" className="w-[100px] h-[100px] bg-[#F2F4F7] rounded-full" />
-                  <div className="rounded-[64px] bg-[#F2F4F7] px-5 py-2 text-center">
-                    <span className="text-[12px] font-semibold text-[#475467]">Upload Photo</span>
+              <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-6 mt-4">
+                <div className="flex flex-col items-start gap-4">
+                  <div className="flex items-center gap-4">
+                    <img src="/userse.png" className="w-[80px] h-[80px] bg-[#F2F4F7] rounded-full" />
+                    <div className="rounded-[64px] bg-[#F2F4F7] px-5 py-2 text-center">
+                      <span className="text-[12px] font-semibold text-[#475467]">Upload Photo</span>
+                    </div>
                   </div>
                 </div>
 
-                <div className="flex flex-col mt-[50px] gap-3">
+                <div className="grid grid-cols-1 lg:grid-cols-1 gap-3 lg:col-span-2">
                   <div>
-                    <label className="block  text-sm font-medium text-[#344054] mb-1">Full Name  </label>
-                
-                    
-                       <div className="w-full h-[44px] text-[#98A2B3] flex items-center text-sm rounded-[12px] border border-[#D0D5DD] p-4">
-                     
-                     {registerData?.data?.name}
+                    <label className="block text-sm font-medium text-[#344054] mb-1">Full Name</label>
+                    <div className="w-full h-[44px] text-[#98A2B3] flex items-center text-sm rounded-[12px] border border-[#D0D5DD] px-4">
+                      {registerData?.data?.name}
                     </div>
                   </div>
                   <div>
-                    <label className="block text-sm font-medium text-[#344054] mb-1">Business Name </label>
-                       <div className="w-full h-[44px] text-[#98A2B3] flex items-center text-sm rounded-[12px] border border-[#D0D5DD] p-4">
-                     
+                    <label className="block text-sm font-medium text-[#344054] mb-1">Business Name</label>
+                    <div className="w-full h-[44px] text-[#98A2B3] flex items-center text-sm rounded-[12px] border border-[#D0D5DD] px-4">
                       {registerData?.data?.business_name}
                     </div>
-                    
                   </div>
                   <div>
-
-                    <div>
-                      <label className="block  text-sm font-medium text-[#344054] mb-1" >Business location  </label>
-                      {/* <Selectbox formik={formik} /> */}
-                       <div className="w-full h-[44px] text-[#98A2B3] flex items-center text-sm rounded-[12px] border border-[#D0D5DD] p-4">
-                     
+                    <label className="block text-sm font-medium text-[#344054] mb-1">Business location</label>
+                    <div className="w-full h-[44px] text-[#98A2B3] flex items-center text-sm rounded-[12px] border border-[#D0D5DD] px-4">
                       {registerData?.data?.business_location}
                     </div>
-                    </div>
                   </div>
                   <div>
-                    <label className="block  text-sm font-medium text-[#344054] mb-1">Email </label>
-                    <div className="w-full h-[44px] text-[#98A2B3] text-sm rounded-[12px] flex items-center border border-[#D0D5DD] p-4">
-                     
-                       {registerData?.data?.email}
+                    <label className="block text-sm font-medium text-[#344054] mb-1">Email</label>
+                    <div className="w-full h-[44px] text-[#98A2B3] text-sm rounded-[12px] flex items-center border border-[#D0D5DD] px-4">
+                      {registerData?.data?.email}
                     </div>
                   </div>
                 </div>
               </div>
             </section>
 
-
             <section className="w-full flex justify-end border-t border-[#F6F6F6] px-4 py-3 sm:px-[20px] sm:py-[12px]">
-
               <button
                 onClick={() => { formik.handleSubmit() }}
                 className="rounded-lg bg-[#685BC7] text-white text-sm font-semibold px-7 py-2"
@@ -194,23 +159,3 @@ const Setting1 = () => {
 }
 
 export default Setting1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
